e3ba7be4948c937f036ce9539be84817233d6d12
khaled sammour <khaled@taplane.com>
2026-01-16 23:47:20 +0300

    remove .dist

--- .dist/usr/local/include/net-snmp/system/freebsd3.h
+++
@@ -1,9 +0,0 @@
-#include "freebsd.h"
-
-#define freebsd2 freebsd2       /* freebsd3 is a superset of freebsd2 */
-
-#undef IFADDR_SYMBOL
-#define IFADDR_SYMBOL "in_ifaddrhead"
-
-#undef PROC_SYMBOL
-#undef NPROC_SYMBOL
